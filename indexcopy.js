bcd0849fe661a852a4bad61cbd998bf12fc0feec
Daniel Østensen <98233180+DanielSan-01@users.noreply.github.com>
2024-02-09 01:18:03 +0100

    Update indexcopy.js

--- indexcopy.js
+++ indexcopy.js
@@ -9,3 +9,90 @@ function increment(){
   gappedcopy.innerText = count 
   console.log(count)
 }
+if (gap >= 2) {
+  overtakeLevel.innerText = "Initiated";
+}
+
+if (gap >= 4) {
+  overtakeLevel.innerText = "Florida man";
+}
+
+if (gap >= 5) {
+  overtakeLevel.innerText = "Madlad";
+}
+
+if (gap >= 7) {
+  overtakeLevel.innerText = "Senior Madlad";
+}
+
+if (gap >= 10) {
+  overtakeLevel.innerText = "Absolute MADMAN";
+}
+
+if (gap >= 15) {
+  overtakeLevel.innerText = "TrueSpeed";
+}
+
+if (gap >= 20) {
+  overtakeLevel.innerText = "Legend";
+}
+
+if (gap >= 23) {
+  overtakeLevel.innerText = "ABSOLUTE LEGEND";
+}
+
+if (gap >= 26) {
+  overtakeLevel.innerText = "HOLY SHIT KILL";
+}
+
+if (gap >= 30) {
+  overtakeLevel.innerText = "STOP THIS MAN";
+}
+
+if (gap >= 32) {
+  overtakeLevel.innerText = "WHOS MANS IS THIS";
+}
+
+if (gap >= 35) {
+  overtakeLevel.innerText = "Ghost rider";
+}
+if (gap >= 35) {
+  overtakeLevel.innerText = "I drive TM";
+}
+
+2 - "Initiated"
+
+
+4 - "Florida man"
+
+
+5 - "Madlad"
+
+
+7 - "Senior Madlad"
+
+
+10 - "Absolute MADMAN"
+
+
+15 - "TrueSpeed"
+
+
+20 - "Legend"
+
+
+23 - "ABSOLUTE LEGEND"
+
+
+26 - "HOLY SHIT KILL"
+
+
+30 - "STOP THIS MAN"
+
+
+32 - "WHOS MANS IS THIS"
+
+
+35 - "Ghost rider"
+
+40 - "I drive TM"
